f90c4708c65993b719ea396eea628294e06cb3f9
HyeongMokJeong <jhm1062@naver.com>
2023-12-29 23:53:28 +0900

    refactor: delete old gateway project written in java

--- api-gateway/src/main/java/com/kjj/apigateway/exception/JwtTokenException.java
+++
@@ -1,9 +0,0 @@
-package com.kjj.apigateway.exception;
-
-import lombok.Getter;
-
-@Getter
-public class JwtTokenException extends RuntimeException{
-    public JwtTokenException(String message) { super(message); }
-
-}
